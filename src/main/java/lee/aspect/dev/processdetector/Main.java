e13903a0fbe17e24e9c5c7be88cd69b92fb80351
lee <108044327+nullpointerexceptionkek@users.noreply.github.com>
2022-12-24 13:56:56 -0800

    added only call once option

--- src/main/java/lee/aspect/dev/processdetector/Main.java
+++ src/main/java/lee/aspect/dev/processdetector/Main.java
@@ -2,11 +2,13 @@ package lee.aspect.dev.processdetector;
 
 import lee.aspect.dev.processdetector.core.windows.ProcessMonitor;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main{
     public static void main(String[] args) {
-        ProcessMonitor pm = new ProcessMonitor();
-        pm.startMonitoring("Notepad.exe", () -> System.out.println("Notepad is open!"));
+
         ProcessMonitor pm2 = new ProcessMonitor();
-        pm2.startMonitoring("chrome.exe", () -> System.out.println("Chrome is open!"));
+        pm2.startMonitoring("chrome.exe", () -> System.out.println("Chrome is open!"),1,TimeUnit.SECONDS);
+
     }
 }
--- src/main/java/lee/aspect/dev/processdetector/core/windows/ProcessMonitor.java
+++ src/main/java/lee/aspect/dev/processdetector/core/windows/ProcessMonitor.java
@@ -1,29 +1,63 @@
 package lee.aspect.dev.processdetector.core.windows;
 
 import lee.aspect.dev.processdetector.core.OpenListener;
+import lee.aspect.dev.processdetector.core.SysUtil;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 
 public class ProcessMonitor {
-    private static OpenListener listener;
-    private static String processName;
+    private String processName;
+    private OpenListener listener;
+    private boolean isProcessOpen;
+    private long waitDuration;
+    private TimeUnit waitDurationUnit;
 
-    public void startMonitoring(String processName, OpenListener listener) {
-        ProcessMonitor.processName = processName;
-        ProcessMonitor.listener = listener;
+    private boolean OnlyCallOnce = true;
+    private final Object lock = new Object();
+
+    public ProcessMonitor(){}
+
+    public ProcessMonitor(String processName, OpenListener listener) {
+        this.processName = processName;
+        this.listener = listener;
+    }
+
+    public void startMonitoring(String processName, OpenListener listener, long waitDuration, TimeUnit waitDurationUnit) {
+        this.processName = processName;
+        this.listener = listener;
+        this.waitDuration = waitDuration;
+        this.waitDurationUnit = waitDurationUnit;
+        this.isProcessOpen = isProcessOpen(processName);
+        if (isProcessOpen && OnlyCallOnce) {
+            listener.onProcessOpen();
+        }
 
         Thread thread = new Thread(() -> {
             while (true) {
-                if (isProcessOpen(processName)) {
-                    listener.onProcessOpen();
-                }
+                synchronized (lock) {
+                    if(OnlyCallOnce){
+                        boolean newIsProcessOpen = isProcessOpen(processName);
+                        if (newIsProcessOpen != isProcessOpen) {
+                            isProcessOpen = newIsProcessOpen;
+                            if (isProcessOpen) {
+                                listener.onProcessOpen();
+                            }
+                        }
+                    }
+                    else{
+                        if (isProcessOpen) {
+                            listener.onProcessOpen();
+                        }
+                    }
 
-                try {
-                    Thread.sleep(1000); // sleep for 1 second
-                } catch (InterruptedException e) {
-                    break;
+                    try {
+                        lock.wait(waitDurationUnit.toMillis(waitDuration));
+                    } catch (InterruptedException e) {
+                        break;
+                    }
                 }
             }
         });
@@ -36,22 +70,25 @@ public class ProcessMonitor {
     }
 
     private static boolean isProcessOpen(String processName) {
-        try {
-            Process process = Runtime.getRuntime().exec("tasklist");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                //System.out.println(line);
-                if (line.contains(processName)) {
-                    return true;
+        if(SysUtil.isWindows()){
+            try {
+                Process process = Runtime.getRuntime().exec("tasklist");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    //System.out.println(line);
+                    if (line.contains(processName)) {
+                        return true;
+                    }
                 }
-            }
 
-            return false;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+                return false;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
         }
+        throw new UnsupportedOperationException("This method is not supported on this operating system.");
     }
 }
 
