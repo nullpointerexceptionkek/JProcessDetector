20dc4626390e9e77e4c80bb483dc62b7af71972f
Aspect99 <lee178lee@gmail.com>
2022-12-24 20:48:47 -0800

    added javadoc and LICENSE.md

--- src/main/java/lee/aspect/dev/processdetector/core/ProcessMonitor.java
+++ src/main/java/lee/aspect/dev/processdetector/core/ProcessMonitor.java
@@ -1,3 +1,28 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package lee.aspect.dev.processdetector.core;
 
 
@@ -5,21 +30,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
-
+/**
+ * monitors a process with a given name and reports its open/close status to a listener.
+ */
 public class ProcessMonitor {
     private final Object lock = new Object();
     private boolean isProcessOpen;
     private boolean processCloseCalled = true;
     private Thread thread;
 
+    /**
+     * Constructs a new `ProcessMonitor` with the specified value for the `processCloseCalled` field.
+     * if `processCloseCalled` is true, the `onProcessClose` method of the listener will not be called if
+     * the program was closed before the `startMonitoring` method was called.
+     * if `processCloseCalled` is false, the `onProcessClose` method of the listener will be called if
+     * the program was closed before the `startMonitoring` method was called.
+     * @param processCloseCalled a boolean indicating whether the `onProcessClose` callback should be called
+     *                           if the process is already closed when the `startMonitoring` method is called.
+     */
     public ProcessMonitor(boolean processCloseCalled) {
         this.processCloseCalled = processCloseCalled;
     }
 
+    /**
+     * Constructs a new `ProcessMonitor` with the `processCloseCalled` field set to true.
+     * the `onProcessClose` method of the listener will not be called if
+     * the program was closed before the `startMonitoring` method was called.
+     */
     public ProcessMonitor() {
     }
 
-    @SuppressWarnings("Duplicates")
+    /**
+     * Determines whether a process with the given name is open.
+     * The method will use isContains() to check if the process name match on Windows
+     * @param processName the name of the process to check
+     * @return true if the process is open, false otherwise
+     * @throws UnsupportedOperationException if the current operating system is not supported
+     */
     private static boolean isProcessOpen(String processName) {
         if (SysUtil.isWindows()) {
             try {
@@ -67,6 +114,14 @@ public class ProcessMonitor {
         throw new UnsupportedOperationException("This method is not supported on this operating system.");
     }
 
+    /**
+     * Starts monitoring the process with the given name.
+     *
+     * @param processName the name of the process to monitor
+     * @param listener the listener to be notified of open/close events
+     * @param waitDuration the duration to wait between checks for the process status
+     * @param waitDurationUnit the unit of time for the waitDuration parameter
+     */
     public void startMonitoring(String processName, OpenCloseListener listener, long waitDuration, TimeUnit waitDurationUnit) {
         if(listener == null) {
             throw new IllegalArgumentException("listener cannot be null");
@@ -102,7 +157,9 @@ public class ProcessMonitor {
         });
         thread.start();
     }
-
+    /**
+     * Stops monitoring the process.
+     */
     public void stopMonitoring() {
         if(thread != null) {
             thread.interrupt();
--- src/main/java/lee/aspect/dev/processdetector/core/SysUtil.java
+++ src/main/java/lee/aspect/dev/processdetector/core/SysUtil.java
@@ -1,13 +1,55 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package lee.aspect.dev.processdetector.core;
 
 public abstract class SysUtil {
 
+    /**
+     * Determine if the current OS is Windows.
+     * detection is not foolproof and might be spoofed.
+     * @return true if the current OS is Windows
+     */
     public static boolean isWindows() {
         return System.getProperty("os.name").toLowerCase().contains("win");
     }
+
+    /**
+     * Determine if the current OS is macOS.
+     * detection is not foolproof and might be spoofed.
+     * @return true if the current OS is macOS
+     */
     public static boolean isMac() {
         return System.getProperty("os.name").toLowerCase().contains("mac");
     }
+
+    /**
+     * Determine if the current OS is Linux.
+     * detection is not foolproof and might be spoofed.
+     * @return true if the current OS is Linux
+     */
     public static boolean isLinux() {
         String osName = System.getProperty("os.name").toLowerCase();
         return osName.contains("nix") || osName.contains("nux") || osName.contains("aix");
